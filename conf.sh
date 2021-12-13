b46c7b604ae57c040afb14acb6bb8b89a773a81a
bugds <bug.dmitrij@gmail.com>
2021-12-13 13:25:53 +0300

    Hypocenter configuration

--- conf.sh
+++ conf.sh
@@ -1,127 +1,87 @@
 # bashgatk.sh
 
-export scriptsDirectory=/home/bioinfuser/NGS/Pipelines/scripts
-export gatk=/opt/gatk-4.1.5.0/gatk
-export samtools=/opt/gatk4-data-processing/samtools-1.3.1/samtools
-export picard=/opt/gatk4-data-processing/picard-2.16.0/picard.jar
-export bwa=/opt/gatk4-data-processing/bwa-0.7.15/bwa
-export refFasta=/home/bioinfuser/NGS/Reference/hg38/hg38.fasta
-export refDict=/home/bioinfuser/NGS/Reference/hg38/hg38.dict
-export dbSnpVcf=/home/bioinfuser/NGS/Reference/hg38/dbsnp138.vcf
-export dbSnpVcfIdx=/home/bioinfuser/NGS/Reference/hg38/dbsnp138.vcf.idx
-export millisVcf=/home/bioinfuser/NGS/Reference/hg38/Mills_and_1000G_gold_standard.indels.hg38.vcf.gz
-export millisVcfIdx=/home/bioinfuser/NGS/Reference/hg38/Mills_and_1000G_gold_standard.indels.hg38.vcf.gz.tbi
-export indelsVcf=/home/bioinfuser/NGS/Reference/hg38/known_indels.hg38.vcf.gz
-export indelsVcfIdx=/home/bioinfuser/NGS/Reference/hg38/known_indels.hg38.vcf.gz.tbi
+export scriptsDirectory=/home/bioinfuser/applications/BashGATK/scripts
+export gatk=/home/bioinfuser/applications/gatk-4.1.8.1/gatk
+export samtools=/home/bioinfuser/applications/samtools-1.3.1/samtools
+export picard=/home/bioinfuser/applications/picard-2.16.0/picard.jar
+export bwa=/home/bioinfuser/applications/bwa-0.7.15/bwa
+export refFasta=/home/bioinfuser/data/hg38/hg38.fasta
+export refDict=/home/bioinfuser/data/hg38/hg38.dict
+export dbSnpVcf=/home/bioinfuser/data/hg38/dbsnp138.vcf
+export dbSnpVcfIdx=/home/bioinfuser/data/hg38/dbsnp138.vcf.idx
+export millisVcf=/home/bioinfuser/data/hg38/Mills_and_1000G_gold_standard.indels.hg38.vcf.gz
+export millisVcfIdx=/home/bioinfuser/data/hg38/Mills_and_1000G_gold_standard.indels.hg38.vcf.gz.tbi
+export indelsVcf=/home/bioinfuser/data/hg38/known_indels.hg38.vcf.gz
+export indelsVcfIdx=/home/bioinfuser/data/hg38/known_indels.hg38.vcf.gz.tbi
 export compressionLevel=5
 export platform=ILLUMINA
 
-if [ $command == 'proc' ]; then
+if [[ $command == 'proc' ]]; then
     # parallelProcessing.sh
-    export fastqc='/opt/FastQC/fastqc'
-    export trimmomatic='java -jar /opt/Trimmomatic-0.39/trimmomatic-0.39.jar'
-    export trimCommandLine='ILLUMINACLIP:/home/bioinfuser/NGS/Reference/adapters/TruSeq3-PE-2.fa:2:30:10'
+    export fastqc='/home/bioinfuser/applications/FastQC/fastqc'
+    export trimmomatic='java -jar /home/bioinfuser/applications/Trimmomatic-0.39/trimmomatic-0.39.jar'
+    export trimCommandLine='ILLUMINACLIP:/home/bioinfuser/applications/Trimmomatic-0.39/adapters/TruSeq3-PE-3.fa:2:30:10'
     export inputFolder=${outputFolder}/fastq/
-    export regions=/home/bioinfuser/NGS/Reference/intervals/2021_03_26/capture_targets.bed
+    export regions=/home/bioinfuser/data/brca_seq/intervals/BRCACNVbr283_ROI.bed
     export bwaVersion="$($bwa 2>&1 | grep -e '^Version' | sed 's/Version: //')"
     export bwaCommandline="$bwa mem -K 100000000 -p -v 3 -t 16 -Y $refFasta"
     export parallelJobs=3
     export lane=1
-elif [ $command == 'procWGS' ]; then
+elif [[ $command == 'procWGS' ]]; then
     # processingWGS.sh
     export inputFolder=${outputFolder}/fastq/
     export bwaVersion="$($bwa 2>&1 | grep -e '^Version' | sed 's/Version: //')"
     export bwaCommandline="$bwa mem -K 100000000 -p -v 3 -t 16 -Y $refFasta"
-elif [ $command == 'somaSNP' ]; then
+elif [[ $command == 'somaSNP' ]]; then
     # parallelSomaticSNP.sh
-    export regions=/home/bioinfuser/NGS/Reference/intervals/2021_03_26/capture_targets.interval_list
-    export refImg=/home/bioinfuser/NGS/Reference/hg38/hg38.fasta.img
-    export gnomad=/home/bioinfuser/NGS/Reference/intervals/2021_03_26/additional/AFonly.vcf
-    export variantsForContamination=/home/bioinfuser/NGS/Reference/intervals/2021_03_26/additional/variants_for_contamination.vcf
+    export regions=/home/bioinfuser/data/MED/intervals/capture_targets.interval_list
+    export refImg=/home/bioinfuser/data/hg38/hg38.fasta.img
+    export gnomad=/home/bioinfuser/data/MED/intervals/additional/AFonly.vcf
+    export variantsForContamination=/home/bioinfuser/data/MED/intervals/additional/variants_for_contamination.vcf
     export parallelJobs=5
     export javaOpt="-Xms3000m"
-elif [ $command == 'germSNP' ]; then
+elif [[ $command == 'germSNP' ]]; then
     # parallelGermlineSNP.sh
-    export regions=/home/bioinfuser/NGS/Reference/intervals/2021_03_26/capture_targets.interval_list
+    export regions=/home/bioinfuser/data/brca_seq/intervals/capture_targets.interval_list
     export parallelJobs=5
     export javaOpt="-Xms3000m"
-elif [ $command == 'anno' ]; then
+elif [[ $command == 'anno' ]]; then
     # annotation.sh
-    export vep=/home/bioinfuser/NGS/Software/ensembl-vep/vep
-    export fasta=/home/bioinfuser/.vep/Homo_sapiens.GRCh38.dna.primary_assembly.fa.gz
-    export inputFolder=${outputFolder}/mutect2/
-    export annovar=/home/bioinfuser/NGS/Software/annovar/table_annovar.pl
-    export annovarDb=/home/bioinfuser/NGS/Software/annovar/humandb/
+    export vep=/home/bioinfuser/applications/ensembl-vep/vep
+    export fasta=/home/bioinfuser/.vep/homo_sapiens/101_GRCh38/Homo_sapiens.GRCh38.dna.primary_assembly.fa
+    export inputFolder=${outputFolder}
+    export annovar=/home/bioinfuser/applications/annovar/table_annovar.pl
+    export annovarDb=/home/bioinfuser/applications/annovar/humandb/
     export buildVer=hg38
-    export protocol='refGene,ensGene,gnomad211_exome,gnomad30_genome,dbnsfp41a,dbscsnv11,clinvar_20210123,cosmic92_coding,cosmic92_noncoding,avsnp150'
+    export protocol='refGene,ensGene,gnomad211_exome,gnomad30_genome,dbnsfp42a,dbscsnv11,clinvar_20210501,cosmic95_coding,cosmic95_noncoding,avsnp150'
     export operation='g,g,f,f,f,f,f,f,f,f'
-    export xreffile=/home/bioinfuser/NGS/Software/annovar/example/gene_fullxref.txt
-elif [ $command == 'qgen' ]; then
-    # annotation.sh
-    export bedtools=/usr/local/bin/bedtools
-    export vep=/home/bioinfuser/NGS/Software/ensembl-vep/vep
-    export fasta=/home/bioinfuser/.vep/homo_sapiens/104_GRCh38/Homo_sapiens.GRCh38.dna.toplevel.fa.gz
-    export chain=/home/bioinfuser/NGS/Reference/hg38/hg19ToHg38.over.chain
-    export annovar=/home/bioinfuser/NGS/Software/annovar/table_annovar.pl
-    export annovarDb=/home/bioinfuser/NGS/Software/annovar/humandb/
-    export buildVer=hg38
-    export protocol='refGene,ensGene,gnomad211_exome,gnomad30_genome,dbnsfp41a,dbscsnv11,clinvar_20210123,cosmic92_coding,cosmic92_noncoding,avsnp150'
-    export operation='g,g,f,f,f,f,f,f,f,f'
-    export xreffile=/home/bioinfuser/NGS/Software/annovar/example/gene_fullxref.txt
-elif [ $command == 'btil' ]; then
+    export xreffile=/home/bioinfuser/applications/annovar/example/gene_fullxref.txt
+elif [[ $command == 'btil' ]]; then
     # bed_to_interval_list.sh
-    export regions=/home/bioinfuser/NGS/Reference/intervals/2021_03_26/capture_targets.bed
-elif [ $command == 'cvfc' ]; then
+    export regions=/home/bioinfuser/data/brca_seq/intervals/BRCACNVbr283_ROI.bed
+elif [[ $command == 'cvfc' ]]; then
     # create_variants_for_contamination.sh
-    export wd=/home/bioinfuser/NGS/Reference/intervals/2021_04_26/additional/
-    export regions=/home/bioinfuser/NGS/Reference/intervals/2021_04_26/depths_hg38.bed
-    export gnomad=/home/bioinfuser/NGS/Reference/hg38/gnomad/gnomad.exomes.r2.1.1.sites.liftover_grch38.vcf
+    export wd=/home/bioinfuser/data/brca_seq/intervals/additional/
+    export regions=/home/bioinfuser/data/brca_seq/intervals/BRCACNVbr283_ROI.bed
+    export gnomad=/home/bioinfuser/data/hg38/gnomad/gnomad.exomes.r2.1.1.sites.liftover_grch38.vcf
     export minimumAlleleFrequency=0.05
     export javaOpt="-Xms3000m"
-elif [ $command == 'cnvk' ]; then
+elif [[ $command == 'cnvk' ]]; then
     # run_cnvkit.sh
-    export cnvkit='python3 /home/bioinfuser/NGS/Software/cnvkit/cnvkit.py'
-    export regions='/home/bioinfuser/NGS/Reference/intervals/2020_10_26/cnvkit_targets.bed'
-    export antitarget='/home/bioinfuser/NGS/Reference/intervals/2020_10_26/cnvkit_antitargets.bed'
-    export mappable='/home/bioinfuser/NGS/Reference/hg38/access-hg38.bed'
-    export refFlat='/home/bioinfuser/NGS/Reference/hg38/hg38.refFlat.txt'
-    export AFonly='/home/bioinfuser/NGS/Reference/intervals/2020_02_02/additional/AFonly.vcf'
+    export cnvkit='python3 /home/bioinfuser/applications/cnvkit/cnvkit.py'
+    export regions='/home/bioinfuser/data/MED/intervals/cnvkit_targets.bed'
+    export antitarget='/home/bioinfuser/data/MED/intervals/cnvkit_antitargets.bed'
+    export mappable='/home/bioinfuser/data/MED/intervals/access-hg38.bed'
+    export refFlat='/home/bioinfuser/data/MED/intervals/hg38.refFlat.txt'
+    export AFonly='/home/bioinfuser/data/MED/intervals/additional/AFonly.vcf'
     export LC_NUMERIC='en_US.UTF-8'
     export javaOpt="-Xms3000m"
-elif [ $command == 'cnvk19' ]; then
-    # run_cnvkit.sh
-    export cnvkit='python3 /home/bioinfuser/NGS/Software/cnvkit/cnvkit.py'
-    export regions='/home/bioinfuser/NGS/Reference/intervals/2021_04_26/cnvkit_targets.bed'
-    export antitarget='/home/bioinfuser/NGS/Reference/intervals/2021_04_26/cnvkit_antitargets.bed'
-    export mappable='/home/bioinfuser/NGS/Reference/hg19/hg19_mappable.bed'
-    export refFlat='/home/bioinfuser/NGS/Reference/hg19/refFlat.txt'
-    export AFonly='/home/bioinfuser/NGS/Reference/intervals/2021_04_26/additional/AFonly_hg19.vcf'
-    export LC_NUMERIC='en_US.UTF-8'
-    export javaOpt="-Xms3000m"
-elif [ $command == 'deep' ]; then
+elif [[ $command == 'deep' ]]; then
     # deepvariant.sh
     export numCpu=16
-    export regions=/home/bioinfuser/NGS/Reference/intervals/2021_03_26/capture_targets.bed
-    export binVersion='0.10.0'
-    export refFolder=/home/bioinfuser/NGS/Reference/
-    export refFastaPathPart=hg38/hg38.fasta
-    export regionsPathPart=intervals/2021_03_26/cnvkit_targets.bed
-elif [ $command == 'deep19' ]; then
-    # deepvariant.sh
-    export numCpu=16
-    export regions=/home/bioinfuser/NGS/Reference/intervals/2021_04_26/depths.bed
-    export binVersion='0.10.0'
-    export refFolder=/home/bioinfuser/NGS/Reference/
-    export refFastaPathPart=hg19/hg19.fa
-    export regionsPathPart=intervals/2021_04_26/depths_hg19.bed
-elif [ $command == 'cnvqiagen' ]; then
-    # run_cnvkit.sh
-    export cnvkit='python3 /home/bioinfuser/NGS/Software/cnvkit/cnvkit.py'
-    export regions='/home/bioinfuser/NGS/Reference/intervals/2021_04_26/cnvkit_targets.bed'
-    export antitarget='/home/bioinfuser/NGS/Reference/intervals/2021_04_26/cnvkit_antitargets.bed'
-    export mappable='/home/bioinfuser/NGS/Reference/hg38/access-hg38.bed'
-    export refFlat='/home/bioinfuser/NGS/Reference/hg38/hg38.refFlat.txt'
-    export AFonly='/home/bioinfuser/NGS/Reference/intervals/2021_04_26/additional/AFonly.vcf'
-    export LC_NUMERIC='en_US.UTF-8'
-    export javaOpt="-Xms3000m"
+    export regions=/home/bioinfuser/data/brca_seq/intervals/BRCACNVbr283_ROI.bed
+    export binVersion='latest'
+    export refFolder=/home/bioinfuser/
+    export refFastaPathPart=data/hg38/hg38.fasta
+    export regionsPathPart=data/brca_seq/intervals/BRCACNVbr283_ROI.bed
 fi
