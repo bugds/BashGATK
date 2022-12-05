59007609c1d04657cf1d8c30e878cc38bf5eb861
bugds <bug.dmitrij@gmail.com>
2022-12-05 08:56:47 +0300

    main example

--- main.sh
+++ main.sh
@@ -3,13 +3,13 @@
 set -e
 set -o pipefail
 
-./bashgatk.sh proc ./confs/conf_bykova_more.sh ../../projects/2022_03_Bykova
+./bashgatk.sh proc ./confs/kapa_hyperexome.sh ../../projects/#
 echo "$(date): Processing completed" >> ./bashgatk.log
-./bashgatk.sh somaSNP ./confs/conf_bykova_more.sh ../../projects/2022_03_Bykova
+./bashgatk.sh somaSNP ./confs/kapa_hyperexome.sh ../../projects/#
 echo "$(date): SomaticSNP ready" >> ./bashgatk.log
-./bashgatk.sh proc ./confs/conf_bykova_less.sh ../../projects/2022_03_Bykova_less
+./bashgatk.sh proc ./confs/kapa_hyperexome.sh ../../projects/#
 echo "$(date): Processing completed" >> ./bashgatk.log
-./bashgatk.sh somaSNP ./confs/conf_bykova_less.sh ../../projects/2022_03_Bykova_less
+./bashgatk.sh somaSNP ./confs/kapa_hyperexome.sh ../../projects/#
 echo "$(date): SomaticSNP ready" >> ./bashgatk.log
 #./bashgatk.sh anno $1 $2
 #echo "$(date): Annotated" >> ./bashgatk.log
