a77f311073596b60ad947fead927938e751a7b67
bugds <bug.dmitrij@gmail.com>
2023-01-17 13:26:10 +0300

    Main example changed

--- main.sh
+++ main.sh
@@ -5,15 +5,19 @@
 set -e
 set -o pipefail
 
-./bashgatk.sh proc ./confs/kapa_hyperexome.sh ../../projects/#
+conf='./confs/kapa_hyperexome.sh'
+
+./bashgatk.sh kumi $conf
 echo "$(date): Processing completed" >> ./bashgatk.log
-./bashgatk.sh somaSNP ./confs/kapa_hyperexome.sh ../../projects/#
+./bashgatk.sh deep $conf
+echo "$(date): DeepVariant ready" >> ./bashgatk.log
+./bashgatk.sh somaSNP $conf
 echo "$(date): SomaticSNP ready" >> ./bashgatk.log
-./bashgatk.sh proc ./confs/kapa_hyperexome.sh ../../projects/#
-echo "$(date): Processing completed" >> ./bashgatk.log
-./bashgatk.sh somaSNP ./confs/kapa_hyperexome.sh ../../projects/#
-echo "$(date): SomaticSNP ready" >> ./bashgatk.log
-#./bashgatk.sh anno $1 $2
-#echo "$(date): Annotated" >> ./bashgatk.log
-#./bashgatk.sh 2csv $1 $2
-#echo "$(date): CSV created" >> ./bashgatk.log
+./bashgatk.sh anno $conf
+echo "$(date): Annotation completed" >> ./bashgatk.log
+./bashgatk.sh 2csv $conf
+echo "$(date): CSV created" >> ./bashgatk.log
+./bashgatk.sh 2rus2 $conf
+echo "$(date): Table created" >> ./bashgatk.log
+./bashgatk.sh mask $conf
+echo "$(date): Masks applied" >> ./bashgatk.log
