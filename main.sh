2f43531b607b053a54b8d9901cc2efb13878f7bf
bugds <bug.dmitrij@gmail.com>
2023-01-18 08:57:28 +0300

    Ignored

--- main.sh
+++ main.sh
@@ -7,12 +7,12 @@ set -o pipefail
 
 conf='./confs/kapa_hyperexome.sh'
 
-./bashgatk.sh kumi $conf
-echo "$(date): Processing completed" >> ./bashgatk.log
-./bashgatk.sh deep $conf
-echo "$(date): DeepVariant ready" >> ./bashgatk.log
-./bashgatk.sh somaSNP $conf
-echo "$(date): SomaticSNP ready" >> ./bashgatk.log
+# ./bashgatk.sh kumi $conf
+# echo "$(date): Processing completed" >> ./bashgatk.log
+# ./bashgatk.sh deep $conf
+# echo "$(date): DeepVariant ready" >> ./bashgatk.log
+# ./bashgatk.sh somaSNP $conf
+# echo "$(date): SomaticSNP ready" >> ./bashgatk.log
 ./bashgatk.sh anno $conf
 echo "$(date): Annotation completed" >> ./bashgatk.log
 ./bashgatk.sh 2csv $conf
