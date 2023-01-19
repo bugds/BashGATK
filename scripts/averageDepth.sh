a4e04af25781388b5d5a79bc46d6442b023a24ff
bugds <bug.dmitrij@gmail.com>
2023-01-19 11:31:41 +0300

    Added some metrics

--- scripts/averageDepth.sh
+++ scripts/averageDepth.sh
@@ -27,8 +27,17 @@ function getDepths {
         $1 \
         -a \
         | \
-        awk '{sum+=$3; sumsq+=$3*$3} END { print "Average = ",sum/NR; print "Stdev = ",sqrt(sumsq/NR - (sum/NR)**2)}' \
+        awk '$3<10{$2=1}1' \
+        | \
+        awk '$3>=10{$2=0}1' \
+        | \
+        awk '{sum+=$3; sumsq+=$3*$3; subsum+=$2} END {print "Average = ",sum/NR; print "Stdev = ",sqrt(sumsq/NR - (sum/NR)**2); print "Sub-ten = ",subsum/NR; print "Total nucleotides: ",NR}' \
         >> ${outputFolder}depths/${bamName}.txt
+    
+    echo "\nTotal reads:" >> ${outputFolder}depths/${bamName}.txt
+    samtools view -c $1 >> ${outputFolder}depths/${bamName}.txt
+    echo "\nMapped reads:" >> ${outputFolder}depths/${bamName}.txt
+    $samtools view -c -F 260 $1 >> ${outputFolder}depths/${bamName}.txt
 }
 
 makeDirectory depths
