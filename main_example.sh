d13da9151e786c29309b32809464e137fa3dcc13
bugds <bug.dmitrij@gmail.com>
2023-03-16 08:43:05 +0300

    Corrected example

--- main_example.sh
+++ main_example.sh
@@ -7,17 +7,13 @@ set -o pipefail
 
 conf='./confs/example.sh'
 
-# ./bashgatk.sh kumi $conf
-# echo "$(date): Processing completed" >> ./bashgatk.log
-# ./bashgatk.sh deep $conf
-# echo "$(date): DeepVariant ready" >> ./bashgatk.log
-# ./bashgatk.sh somaSNP $conf
-# echo "$(date): SomaticSNP ready" >> ./bashgatk.log
+./bashgatk.sh kumi $conf
+echo "$(date): Processing completed" >> ./bashgatk.log
+./bashgatk.sh deep $conf
+echo "$(date): DeepVariant ready" >> ./bashgatk.log
+./bashgatk.sh somaSNP $conf
+echo "$(date): SomaticSNP ready" >> ./bashgatk.log
 ./bashgatk.sh anno $conf
 echo "$(date): Annotation completed" >> ./bashgatk.log
 ./bashgatk.sh 2csv $conf
 echo "$(date): CSV created" >> ./bashgatk.log
-./bashgatk.sh 2rus2 $conf
-echo "$(date): Table created" >> ./bashgatk.log
-./bashgatk.sh mask $conf
-echo "$(date): Masks applied" >> ./bashgatk.log
